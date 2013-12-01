a3116ebe497e5bb5382c3ebdb67a0df57398f802
a-know <a.know.3373@gmail.com>
2013-12-01 09:05:49 +0900

    Using the Users Service / the Go Getting Started Guide - Google App Engine

--- hello/hello.go
+++ hello/hello.go
@@ -3,12 +3,28 @@ package hello
 import (
 	"fmt"
 	"net/http"
+
+	"appengine"
+	"appengine/user"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Go言語はじめてみた！！！ by a-know")
+func handler(res http.ResponseWriter, req *http.Request) {
+	// returns an appengine.Context value associated with the current request
+	context := appengine.NewContext(req)
+	u := user.Current(context)
+	if u == nil {
+		url, err := user.LoginURL(context, req.URL.String())
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		res.Header().Set("Location", url)
+		res.WriteHeader(http.StatusFound)
+		return
+	}
+	fmt.Fprintf(res, "こんにちは, %vさん！", u)
 }
