7d9e3157572a51b388471554b4d82a64c413ef59
a-know <a.know.3373@gmail.com>
2013-12-01 10:40:23 +0900

    Using the Datastore / the Go Getting Started Guide - Google App Engine

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,20 @@
 package hello
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
+	"time"
+
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
 )
+
+type Greeting struct {
+	Author  string
+	Content string
+	Date    time.Time
+}
 
 func init() {
 	http.HandleFunc("/", root)
@@ -12,34 +22,61 @@ func init() {
 }
 
 func root(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(res, guestbookForm)
-}
-
-const guestbookForm = `
-<html>
-  <body>
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="送信"></div>
-    </form>
-  </body>
-</html>
-`
-
-func sign(res http.ResponseWriter, req *http.Request) {
-	err := signTemplate.Execute(res, req.FormValue("content"))
-	if err != nil {
+	context := appengine.NewContext(req)
+	// ancestor query を発行
+	q := datastore.NewQuery("Greeting").Ancestor(guestbookKey(context)).Order("-Date").Limit(10)
+	// サイズ10のsliceを作成
+	greetings := make([]Greeting, 0, 10)
+	if _, err := q.GetAll(context, &greetings); err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := guestbookTemplate.Execute(res, greetings); err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-var signTemplate = template.Must(template.New("contents").Parse(signTemplateHTML))
+// 全 Greeting エンティティに設定する親keyを返す
+func guestbookKey(context appengine.Context) *datastore.Key {
+	// 親keyの作成。stringIDは "default_guestbook" で固定
+	return datastore.NewKey(context, "Guestbook", "default_guestbook", 0, nil)
+}
 
-const signTemplateHTML = `
+func sign(res http.ResponseWriter, req *http.Request) {
+	context := appengine.NewContext(req)
+	g := Greeting{
+		Content: req.FormValue("content"),
+		Date:    time.Now(),
+	}
+	if u := user.Current(context); u != nil {
+		g.Author = u.String()
+	}
+	key := datastore.NewIncompleteKey(context, "Greeting", guestbookKey(context))
+	_, err := datastore.Put(context, key, &g)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(res, req, "/", http.StatusFound)
+}
+
+var guestbookTemplate = template.Must(template.New("book").Parse(guestbookTemplateHTML))
+
+const guestbookTemplateHTML = `
 <html>
   <body>
-    <p>送信内容:</p>
-    <pre>{{.}}</pre>
+    {{range .}}
+      {{with .Author}}
+        <p><b>{{.}}</b> wrote:</p>
+      {{else}}
+        <p>An anonymous person wrote:</p>
+      {{end}}
+      <pre>{{.Content}}</pre>
+    {{end}}
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
   </body>
 </html>
 `
